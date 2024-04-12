06f7715e5cd494c8ed71a097de01c3cbcb3ea061
donaldwongsuisang <donaldwongsuisang@gmail.com>
2024-04-12 14:04:04 +0800

    biild: route & controller setup

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/',(req, res) =>{
   res.send('express app for URL_shortener')
 })
 
+app.get('/:shorten_id', (req, res) =>{
+const shorten_id = req.params.shorten_id
+  res.send(`shortener id: ${shorten_id}`)
+})
 app.listen(port, ()=>{
   console.log(`express service is running on http://localhost:${port}`)
 })
