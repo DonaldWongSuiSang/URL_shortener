942a3fec3e2090814de664e4c22625d838d55716
donaldwongsuisang <donaldwongsuisang@gmail.com>
2024-04-16 17:37:58 +0800

    modify the fomat and leave comment

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const fs = require('fs')
+
+// Path to the JSON file
 const Path = ('./public/jsons/shorten.json')
 const jsonData = fs.readFileSync(Path, 'utf8')
 const existingData = JSON.parse(jsonData)
@@ -9,36 +11,35 @@ const existingData = JSON.parse(jsonData)
 const app = express()
 const port = 3000
 
-
-
 //setup handlerbars
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views','./views')
 
-
+// static file & Body parser middleware for parsing form data
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json()) // for parsing application/json
 
-
+// Route to render the index page
 app.get('/',(req, res) =>{
-  res.render('index', {showInput: true})
+  res.render('index', {showInput: true})  // Pass 'showInput' variable to indicate whether to show the input form or not
 })
 
+// Route to handle form submission and URL shortening
 app.post('/shorten', (req, res) =>{
   const originalUrl = req.body.originalUrl
-  console.log("shorten2: ", req.body)
   const shortUrl = generateShortUrl(originalUrl)
   saveShortUrl(originalUrl, shortUrl)
-  res.render('index',{port, originalUrl,shortUrl, showInput:false})
+  res.render('index',{port, originalUrl,shortUrl, showInput:false}) // Pass data to render the index page with the shortened URL
 })
 
+// Route to handle redirecting short URLs to original URLs
 app.get('/:shortUrl',(req,res) =>{
   const shortUrl = req.params.shortUrl
   const originalUrl = getOriginalUrl(shortUrl)
   if(originalUrl){
-       res.redirect(originalUrl)
+    res.redirect(originalUrl)
   }
   else{
     res.status(404).send('URL not found')
@@ -46,31 +47,32 @@ app.get('/:shortUrl',(req,res) =>{
 })
 
 
-
+// Function to generate a short URL
 function generateShortUrl(originalUrl) {
    const existingUrl = existingData.results.find(item => item.originalUrl === originalUrl)
-if(existingUrl){
-  return existingUrl.shortUrl
-}else{
-    const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  if(existingUrl){
+    return existingUrl.shortUrl
+  }else{
+    const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     let shortUrl = ''
     for (let i = 0; i < 5; i++) {
       shortUrl += characters[Math.floor(Math.random() * characters.length)]
     }
-    return shortUrl; 
-}
+    return shortUrl
+  }
 }
 
+// Function to save a new short URL to the JSON file
 function saveShortUrl(originalUrl, shortUrl) {
-  // Save to JSON file logic here
-     const existingUrl = existingData.results.find(item => item.originalUrl === originalUrl)
-     if(!existingUrl){
-  existingData.results.push({originalUrl, shortUrl})
-  fs.writeFileSync(Path, JSON.stringify(existingData))}
+  const existingUrl = existingData.results.find(item => item.originalUrl === originalUrl)
+  if(!existingUrl){
+    existingData.results.push({originalUrl, shortUrl})
+    fs.writeFileSync(Path, JSON.stringify(existingData))
+  }
 }
 
+// Function to retrieve the original URL for a given short URL
 function getOriginalUrl(shortUrl) {
-  // Retrieve original URL from JSON file logic here
   const result = existingData.results.find(item => item.shortUrl === shortUrl)
   if (result) {
     let originalUrl = result.originalUrl
@@ -83,6 +85,7 @@ function getOriginalUrl(shortUrl) {
  }
 }
 
+// Start the server
 app.listen(port, ()=>{
   console.log(`express service is running on http://localhost:${port}`)
 })
